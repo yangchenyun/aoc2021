27ba328c36c6dff25f3126bada1a917b580a9952
Steven Yang <yangchenyun@gmail.com>
2021-12-10 01:27:20 -0800

    solved day3.part2

--- day3/main.go
+++ day3/main.go
@@ -74,6 +74,82 @@ func main() {
 	e2 := int(^r&m)
 	fmt.Printf("Result 1: %b, %b, %v\n", g2, e2, g2*e2)
 
+	fmt.Printf("Result 2: %b\n", filterOxy(numbers, leng - 1))
+	fmt.Printf("Result 2: %b\n", filterCo2(numbers, leng - 1))
+	fmt.Printf("Result 2: %v\n",
+		int(filterOxy(numbers, leng - 1))* int(filterCo2(numbers, leng - 1)))
+
+}
+
+func filterOxy(numbers []uint16, i int) uint16{
+	if len(numbers) == 1 {
+		return numbers[0]
+	}
+
+	one_c := 0
+	zero_c := 0
+	for _, n := range(numbers) {
+		if (n>>i)&1 == 0b1 {
+			one_c++
+		} else {
+			zero_c++
+		}
+	}
+
+	var k int
+	if one_c > zero_c {
+		k = 1
+	} else if zero_c > one_c {
+		k = 0
+	} else {
+		k = 1
+	}
+
+	new_numbers := make([]uint16, 0)
+	for _, n := range(numbers) {
+		if (n>>i)&1 == uint16(k) {
+			new_numbers = append(new_numbers, n)
+		}
+	}
+	return filterOxy(new_numbers, i - 1)
+}
+
+func filterCo2(numbers []uint16, i int) uint16{
+	if len(numbers) == 0 {
+		panic("Unexpected empty numbers.")
+	}
+
+	if len(numbers) == 1 {
+		return numbers[0]
+	}
+
+	one_c := 0
+	zero_c := 0
+	for _, n := range(numbers) {
+		if (n>>i)&1 == 0b1 {
+			one_c++
+		} else {
+			zero_c++
+		}
+	}
+
+	var k int
+	if one_c > zero_c {
+		k = 0
+	} else if zero_c > one_c {
+		k = 1
+	} else {
+		k = 0
+	}
+
+	new_numbers := make([]uint16, 0)
+	for _, n := range(numbers) {
+		if (n>>i)&1 == uint16(k) {
+			// fmt.Printf("%b, %b", n, (n>>i)&1)
+			new_numbers = append(new_numbers, n)
+		}
+	}
+	return filterCo2(new_numbers, i - 1)
 }
 
 func mask(l int) uint16 {
