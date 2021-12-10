e9ab317cef9252cc35e3c84878c1ba4a140d116b
Steven Yang <yangchenyun@gmail.com>
2021-12-10 01:05:04 -0800

    solved day3.part1 (data issue T-T)

--- day3/main.go
+++ day3/main.go
@@ -16,13 +16,13 @@ func main() {
     defer file.Close()
 
 	var leng int
-	numbers := make([]uint64, 0)
+	numbers := make([]uint16, 0)
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
 		s := scanner.Text()
 		leng = len(s)
 		n, _ := strconv.ParseUint(s, 2, leng)
-		numbers = append(numbers, n)
+		numbers = append(numbers, uint16(n))
     }
 
     if err := scanner.Err(); err != nil {
@@ -31,12 +31,14 @@ func main() {
 
 	m := mask(leng)
 	digits := make([]uint16, leng)
+	gamma := ""
+	epsilon := ""
 
 	for i := leng - 1; i >= 0; i-- {
 		one_c := 0
 		zero_c := 0
 		for _, n := range(numbers) {
-			if (n>>i) & 1 == 0b1 {
+			if (n>>i)&1 == 0b1 {
 				one_c++
 			} else {
 				zero_c++
@@ -45,8 +47,12 @@ func main() {
 		// NOTE: digits saved in order
 		if one_c > zero_c {
 			digits = append(digits, 1)
+			gamma += "1"
+			epsilon += "0"
 		} else if zero_c > one_c {
 			digits = append(digits, 0)
+			gamma += "0"
+			epsilon += "1"
 		} else {
 			log.Printf("Unexpecte results %v, %v", zero_c, one_c)
 		}
@@ -59,9 +65,15 @@ func main() {
 	for _, d := range(digits) {
 		r = (r<<1)+d
 	}
-	gamma := int(r)
-	epsilon := int(^r&m)
-	fmt.Printf("%b, %b, %v", gamma, epsilon, gamma*epsilon)
+
+	g, _ := strconv.ParseInt(gamma, 2, 64)
+	e, _ := strconv.ParseInt(epsilon, 2, 64)
+	fmt.Printf("Result 1: %b, %b, %v\n", g, e, g*e)
+
+	g2 := int(r)
+	e2 := int(^r&m)
+	fmt.Printf("Result 1: %b, %b, %v\n", g2, e2, g2*e2)
+
 }
 
 func mask(l int) uint16 {
